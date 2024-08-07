dd892d8178b7cfa9786373a0e9efd1e97bfe48c3
kroissenbrunner.martin <kroissenbrunner.martin@gmail.com>
2024-08-07 21:49:17 +0200

    Initialer commit mit Daten von neuer cookiecutter Version

---
+++ fetch_data.sh
@@ -0,0 +1,15 @@
+# bin/bash
+
+echo "fetching transkriptions from data_repo"
+rm -rf data/editions && mkdir data/editions
+rm -rf data/indices && mkdir data/indices
+rm -rf data/meta && mkdir data/meta
+curl -LO https://github.com/nuntiaturberichte/nbr-graz-data/archive/refs/heads/main.zip
+unzip main
+
+mv ./nbr-graz-data-main/data/editions/ ./data
+mv ./nbr-graz-data-main/data/indices/ ./data
+mv ./nbr-graz-data-main/data/meta/ ./data
+
+rm main.zip
+rm -rf ./nbr-graz-data-main
