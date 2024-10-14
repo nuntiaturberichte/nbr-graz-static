0fc8db31f7956efadbc066c276febf422246030d
kroissenbrunner.martin <kroissenbrunner.martin@gmail.com>
2024-10-14 13:39:16 +0200

    an meinen fall angepasst

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,15 +1,28 @@
-# bin/bash
+#!/bin/bash
 
-echo "fetching transkriptions from data_repo"
-rm -rf data/editions && mkdir data/editions
-rm -rf data/indices && mkdir data/indices
-rm -rf data/meta && mkdir data/meta
+echo "Fetching transcriptions from data_repo"
+
+# Lösche und erstelle neue Verzeichnisse für die Daten
+rm -rf data/editions && mkdir -p data/editions
+rm -rf data/fronts && mkdir -p data/fronts
+rm -rf data/meta && mkdir -p data/meta
+rm -rf data/mets && mkdir -p data/mets
+rm -rf data/register && mkdir -p data/register
+
+# Lade das Repository als ZIP-Archiv herunter
 curl -LO https://github.com/nuntiaturberichte/nbr-graz-data/archive/refs/heads/main.zip
-unzip main
 
-mv ./nbr-graz-data-main/data/editions/ ./data
-mv ./nbr-graz-data-main/data/indices/ ./data
-mv ./nbr-graz-data-main/data/meta/ ./data
+# Entpacke das heruntergeladene ZIP-Archiv
+unzip main.zip
 
+# Verschiebe die benötigten Ordner aus dem tieferen Verzeichnis in die entsprechenden Verzeichnisse unter "data"
+mv ./nbr-graz-data-main/nbr-graz-data-main/editions/* ./data/editions/
+mv ./nbr-graz-data-main/nbr-graz-data-main/meta/* ./data/meta/
+mv ./nbr-graz-data-main/nbr-graz-data-main/mets/* ./data/mets/
+mv ./nbr-graz-data-main/nbr-graz-data-main/register/* ./data/register/
+
+# Bereinige: Lösche das heruntergeladene ZIP-Archiv und das entpackte Verzeichnis
 rm main.zip
 rm -rf ./nbr-graz-data-main
+
+echo "Transcriptions have been successfully updated."
