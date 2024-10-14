115f5372b0382acd98b08e00b865e5ae381738be
csae8092 <p.andorfer@gmail.com>
2024-10-14 14:25:12 +0200

    fix permissions; changes in fetch_data (as it did not work properly on my machine)

--- fetch_data.sh
+++ fetch_data.sh
@@ -3,11 +3,7 @@
 echo "Fetching transcriptions from data_repo"
 
 # Lösche und erstelle neue Verzeichnisse für die Daten
-rm -rf data/editions && mkdir -p data/editions
-rm -rf data/fronts && mkdir -p data/fronts
-rm -rf data/meta && mkdir -p data/meta
-rm -rf data/mets && mkdir -p data/mets
-rm -rf data/register && mkdir -p data/register
+rm -rf data/ && mkdir data
 
 # Lade das Repository als ZIP-Archiv herunter
 curl -LO https://github.com/nuntiaturberichte/nbr-graz-data/archive/refs/heads/main.zip
@@ -15,14 +11,27 @@ curl -LO https://github.com/nuntiaturberichte/nbr-graz-data/archive/refs/heads/m
 # Entpacke das heruntergeladene ZIP-Archiv
 unzip main.zip
 
-# Verschiebe die benötigten Ordner aus dem tieferen Verzeichnis in die entsprechenden Verzeichnisse unter "data"
-mv ./nbr-graz-data-main/nbr-graz-data-main/editions/* ./data/editions/
-mv ./nbr-graz-data-main/nbr-graz-data-main/meta/* ./data/meta/
-mv ./nbr-graz-data-main/nbr-graz-data-main/mets/* ./data/mets/
-mv ./nbr-graz-data-main/nbr-graz-data-main/register/* ./data/register/
+# # Verschiebe die benötigten Ordner aus dem tieferen Verzeichnis in die entsprechenden Verzeichnisse unter "data"
+mv ./nbr-graz-data-main/* ./data
 
-# Bereinige: Lösche das heruntergeladene ZIP-Archiv und das entpackte Verzeichnis
+
+# # Bereinige: Lösche das heruntergeladene ZIP-Archiv und das entpackte Verzeichnis
 rm main.zip
 rm -rf ./nbr-graz-data-main
 
 echo "Transcriptions have been successfully updated."
+
+#!/bin/bash
+
+REDMINE_ID="18716?format=xhtml&locale="
+IMPRINT_XML=./data/imprint.xml
+rm ${IMPRINT_XML}
+echo '<?xml version="1.0" encoding="UTF-8"?>'
+echo "<root>" >> ${IMPRINT_XML}
+echo '<div lang="de">' >> ${IMPRINT_XML}
+curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}de >> ${IMPRINT_XML}
+echo "</div>"  >> ${IMPRINT_XML}
+echo '<div lang="en">' >> ${IMPRINT_XML}
+curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}en >> ${IMPRINT_XML}
+echo "</div>" >> ${IMPRINT_XML}
+echo "</root>" >> ${IMPRINT_XML}
